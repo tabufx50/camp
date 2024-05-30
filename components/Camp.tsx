c9ac526ffaec82df48ad067ea459e9875c1b0bad
tabufx50 <tabufx50@hotmail.com>
2024-05-29 17:54:23 -0700

    get app section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,4 @@
+import { PEOPLE_URL } from "@/constants";
 import Image from "next/image";
 
 interface CampProps {
@@ -20,8 +21,26 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =
                  width={28}
                  height={28}
                  />
-
             </div>
+            <div className="flex flex-col gap-1">
+              <h4 className="bold-18 text-white">{title}</h4>
+              <p className="regular-14 text-white">{subtitle}</p>
+            </div>
+          </div>
+          <div className="flexCenter gap-6">
+              <span className="flex -space-x-4 overflow-hidden">
+                {PEOPLE_URL.map((url) => (
+                  <Image
+                  src={url}
+                  key={url}
+                  alt="person"
+                  width={52}
+                  height={52}
+                  />
+
+                ))}
+              </span>
+              <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
           </div>
       </div>
     </div>
@@ -30,7 +49,7 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =
 
 const Camp = () => {
   return (
-    <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+    <section className='2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
       <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
       <CampSite
         backgroundImage="bg-bg-img-1"
@@ -38,9 +57,31 @@ const Camp = () => {
         subtitle="Prigen, Pasuruan"
         peopleJoined="50+ Joined"
       />
-      <CampSite />
+      <CampSite
+      backgroundImage="bg-bg-img-2"
+        title="Mountain View Camp"
+        subtitle="Somewhere in the wild"
+        peopleJoined="50+ Joined" />
       </div>
 
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"><strong>Feeling Lost </strong>
+            And Not Knowing The Way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+          Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure.
+          </p>
+          <Image
+          src="/quote.svg"
+          alt='camp-2'
+          width={186}
+          height={219}
+          className="camp-quote"
+          />
+        </div>
+
+      </div>
     </section>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14 '>
+
+      </div>
+      
+      </footer>
   )
 }
 
